e80fcaf7ab140cd666b708070626518b7538bfac
andreasberg-se <95615001+andreasberg-se@users.noreply.github.com>
2022-02-03 14:04:03 +0100

    Add files via upload

--- AspMvc/Controllers/HomeController.cs
+++ AspMvc/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AspMvc.Models;
+using AspMvc.Models.Repositories;
 
 namespace AspMvc.Controllers
 {
